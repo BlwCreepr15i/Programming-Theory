e22b8b4cd3ef9717eb20246638d8d5212272dd9b
BlwCreepr15i <F3510429693@outlook.com>
2026-05-16 21:36:16 +0800

    Created MenuUIHandler.cs and introduced a method for loading Game scene

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
